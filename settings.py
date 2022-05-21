58d55b681a6cfcb8d175ba6b989b69ddcf772eab
Buterbrod <_star@mail.ru>
2022-05-22 00:29:37 +0300

    Updated telegram support

---
+++ settings.py
@@ -0,0 +1,40 @@
+book_channels = """
+[
+    {
+        "name": "Data Science Books",
+        "link": "https://t.me/DataScience_Books"
+    },
+    {
+        "name": "Javascript js frontend",
+        "link": "https://t.me/frontendarchive"
+    },
+    {
+        "name": "Книги для программиста", 
+        "link": "https://t.me/bookofgeek",
+    },
+    {
+        "name": "Data Science Books (Backup Channel)", 
+        "link": "https://t.me/+KTJB5MeJzJRhMDk8",
+    },
+        {
+        "name": "DLStories | Нейронные сети и ИИ", 
+        "link": "https://t.me/dl_stories"
+    },
+        {
+        "name": "Physics.Math.Code", 
+        "link": "https://t.me/physics_lib"
+    },
+        {
+        "name": "Архив программиста", 
+        "link": "https://t.me/techrocksarchive"
+    },
+    {
+        "name": "Архив книг по Базам данным", 
+        "link": "https://t.me/dblib"
+    },
+    {
+        "name": "Java библиотека", 
+        "link": "https://t.me/javalib"
+    }
+] 
+"""
